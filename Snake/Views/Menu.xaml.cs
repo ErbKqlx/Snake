9addd1141eebf837f4174860cd4bd53f7ad8b013
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2025-02-07 14:07:37 +0500

    Добавлено главное меню

---
+++ Snake/Views/Menu.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Snake.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Button button = sender as Button;
+
+            //button.Background = Brushes.White;
+            //button.BorderBrush = Brushes.Purple;
+            //button.Foreground = Brushes.Black;
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //Button button = sender as Button;
+            //button.Background = Brushes.White;
+            //button.BorderBrush = Brushes;
+            //button.Foreground = Brushes.White;
+        }
+    }
+}
